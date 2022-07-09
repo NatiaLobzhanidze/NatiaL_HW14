d861ffc5134da62125da224e758117e0bf2c82d1
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-07-09 23:17:29 +0400

    first page - login

--- NatiaL_HW14/ViewController.swift
+++ NatiaL_HW14/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signInBtn.layer.cornerRadius = 15
+        
+        
     }
 
 
